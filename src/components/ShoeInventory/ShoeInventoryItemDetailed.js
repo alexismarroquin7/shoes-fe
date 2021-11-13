f63f070cb8d10316f92bb9ae75b5a06e0acdf1c1
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-11-13 14:50:04 -0800

    added style to shoe inventory item

--- src/components/ShoeInventory/ShoeInventoryItemDetailed.js
+++ src/components/ShoeInventory/ShoeInventoryItemDetailed.js
@@ -1,18 +1,27 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { ShoeInventory as Action } from "../../store";
 
 export const ShoeInventoryItemDetailed = () => {
   const dispatch = useDispatch();
   const params = useParams();
+  const inventory = useSelector(s => s.inventory);
   
+  const [shoe] = inventory.list;
+
   useEffect(() => {
     dispatch(Action.findByShoeId(params.shoe_id));
   }, [dispatch, params.shoe_id]);
 
   return (
     <div>
+      {shoe && (
+        <div>
+          <h3>{shoe.brand.name}</h3>
+          <h3>{shoe.name}</h3>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/ShoeInventory/ShoeInventoryList.js
+++ src/components/ShoeInventory/ShoeInventoryList.js
@@ -21,9 +21,16 @@ export const ShoeInventoryList = () => {
     <div
       style={{
         display: "flex",
-        flexFlow: "column wrap"
+        flexFlow: "column wrap",
+        alignItems: "center",
+        width: "100%"
       }}
     >
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+
       {inventory.list.length > 0 && inventory.list.map(inventory_item => {
         return ( <ShoeInventoryItem key={inventory_item.shoe_id} inventory_item={inventory_item}/> )
       })}
